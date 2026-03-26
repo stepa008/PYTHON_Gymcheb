621f93bf4f90de752e4d2ada3455a4fb99e34524
stepa008 <stnemec@gymcheb.cz>
2026-03-26 12:07:54 +0100

    Add user input and calculations for tasks

--- Lekce_02_Input/main.py
+++ Lekce_02_Input/main.py
@@ -2,15 +2,24 @@
 # ÚLOHA 1: Registrace (jméno a město)
 # ==========================================
 # Sem napiš svůj kód:
-
-
+jmeno=input("Zadej své jméno: ")
+barva = input("Zadej svou oblíbenou barvu: ")
+print(f"Ahoj {jmeno}, tvoje oblíbená barva je {barva}. To je skvělá volba!")
+print()
 # ==========================================
 # ÚLOHA 2: Jednoduchá kalkulačka (součet)
 # ==========================================
 # Sem napiš svůj kód:
-
-
+cislo1 = int(input("Zadej první číslo: "))
+cislo2 = int(input("Zadej druhé číslo: "))
+soucet = cislo1 + cislo2
+print(f"Součet je: {soucet}")
+print()
 # ==========================================
 # ÚLOHA 3: Výpočet psího věku (věk * 7)
 # ==========================================
 # Sem napiš svůj kód:
+vek = int(input("Kolik ti je let: "))
+psi_vek=vek * 7
+print(f"V psích letech by ti bylo: {psi_vek} let. Haf!")
+print()
